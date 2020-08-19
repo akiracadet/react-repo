b871c90f673f3d3d55f54ba7a6711acce0466227
Mavis Vermillion <mavis.vermillion.ichi@gmail.com>
2020-08-20 00:54:55 +0800

    Hello World Second

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './001/App';
+import App from './002/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
